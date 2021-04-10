d13b711ae88623789c337b606c52b4b51d949433
Maxson Green <maxson.green@gmail.com>
2021-04-09 22:07:35 -0400

    added initial get routes and fixed filepathing issues

---
+++ Develop/public/assets/routes/routes.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = app => {
+
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../notes.html"));
+        });
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../index.html"));
+      });
+
+
+}
